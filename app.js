46d24e786224c77679c5be4cc3b1ecaf2d25d777
Joanna Skrzypczak <j.skrzypczak1998@gmail.com>
2022-12-07 00:26:14 +0100

    Changed px to rem

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const menuButton = document.querySelector(".menu-button");
 const menuButtonIcon = document.querySelector(".menu-icon");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
+const body = document.querySelector("body");
 
 const showMenu = () => {
   hamburgerMenu.classList.add("is-active");
-  menuButtonIcon.src="./images/icon-close.svg";
-}
+  menuButtonIcon.src = "./images/icon-close.svg";
+};
 const closeMenu = () => {
   hamburgerMenu.classList.remove("is-active");
-  menuButtonIcon.src="./images/icon-hamburger.svg";
-}
-menuButton.addEventListener("click", () => hamburgerMenu.classList.contains("is-active") ? closeMenu() : showMenu());
+  menuButtonIcon.src = "./images/icon-hamburger.svg";
+};
+menuButton.addEventListener("click", () =>
+  hamburgerMenu.classList.contains("is-active") ? closeMenu() : showMenu()
+);
